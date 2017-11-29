565a18441ba586876e19af1797f202aa5f1a017c
myhuasen <huanshen1579791973@hotmail.com>
2017-11-29 13:48:23 +0800

    阵列洗牌
    用了模板库和命名空间

---
+++ Array shuffle.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+namespace hh {
+	template <typename T>
+	void roll(T *y, int x)
+	{
+		srand(time(NULL));
+		int i = x, j;
+		T tmp;
+		if (x == 0)
+			return;
+		while (--i)
+		{
+			j = rand() % (i + 1);
+			tmp = y[j];
+			y[j] = y[i];
+			y[i] = tmp;
+		}
+	}
+}
